077db92818356af19468871569545ca8956617bb
Jakub Martin <jakub.wit.martin@gmail.com>
2020-03-29 12:48:18 +0200

    Adding the environment through MonadReader

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import qualified Data.Map as Map
+import Control.Monad.Reader
 
 import Language.Lex
 import Language.Par
@@ -14,26 +15,31 @@ sub (Integer v1) (Integer v2) = Integer (v1 - v2)
 multiply (Integer v1) (Integer v2) = Integer (v1 * v2)
 divide (Integer v1) (Integer v2) = Integer (v1 `div` v2)
 
-data Env = Env (Map.Map Ident Value) deriving (Show)
-startEnv = Env (Map.empty)
-withVariable ident val (Env vars) = Env (Map.insert ident val vars)
-getVariable ident (Env vars) = vars Map.! ident
+data Env = Env { variables :: Map.Map Ident Value } deriving (Show)
+type R a = Reader Env a
 
-interpretExpr :: Expr -> Env -> Value
-interpretExpr x env = case x of
-  EAdd expr0 expr1  -> add (interpretExpr expr0 env) (interpretExpr expr1 env)
-  ESub expr0 expr1  -> sub (interpretExpr expr0 env) (interpretExpr expr1 env)
-  EMul expr0 expr1  -> multiply (interpretExpr expr0 env) (interpretExpr expr1 env)
-  EDiv expr0 expr1  -> divide (interpretExpr expr0 env) (interpretExpr expr1 env)
-  EInt n -> Integer n
-  ETrue -> Bool True 
-  EFalse -> Bool False
-  EVar ident -> getVariable ident env
-  ESemicolon stmt expr -> (interpretExpr expr) . (interpretStmt stmt) $ env
+startEnv = Env { variables = Map.empty }
+withVariable :: Ident -> Value -> Env -> Env
+withVariable ident val env = env { variables = Map.insert ident val (variables env) }
 
-interpretStmt :: Stmt -> Env -> Env
-interpretStmt stmt env = case stmt of
-  SDeclVar ident expr -> withVariable ident (interpretExpr expr env) env
+getVariable :: Ident -> R Value
+getVariable ident = (Map.!) <$> (asks variables) <*> pure ident
+
+interpretExpr :: Expr -> R Value
+interpretExpr x = case x of
+  EAdd expr0 expr1  -> liftM2 add (interpretExpr expr0) (interpretExpr expr1)
+  ESub expr0 expr1  -> liftM2 sub (interpretExpr expr0) (interpretExpr expr1)
+  EMul expr0 expr1  -> liftM2 multiply (interpretExpr expr0) (interpretExpr expr1)
+  EDiv expr0 expr1  -> liftM2 divide (interpretExpr expr0) (interpretExpr expr1)
+  EInt n -> pure . Integer $ n
+  ETrue -> pure . Bool $ True
+  EFalse -> pure . Bool $ False
+  EVar ident -> getVariable ident
+  ESemicolon stmt expr -> (interpretStmt stmt) >>= ((flip local) (interpretExpr expr))
+
+interpretStmt :: Stmt -> R (Env -> Env)
+interpretStmt stmt = case stmt of
+  SDeclVar ident expr -> (withVariable ident) <$> (interpretExpr expr)
 
 main = do
     interact calc
@@ -41,4 +47,4 @@ main = do
 
 calc s =
     let Ok e = pExpr (myLexer s)
-    in show (interpretExpr e startEnv)
+    in show (runReader (interpretExpr e) startEnv)
--- repl.hs
+++ repl.hs
@@ -2,5 +2,5 @@ let Ok e = pExpr (myLexer "(2+2)*5") in e
 startEnv
 calc "(2+2)*5"
 pExpr (myLexer "val x = 6; val y = 2; 2")
-withVariable (Ident "test") (Integer 5) startEnv
+calc "val x = 6; val y = 2; 2"
 calc "val x = 6; val y = 2; x * y"
