40249dde7cf685c6fcc71350dd75d41cccbba1f6
Jakub Martin <jakub.wit.martin@gmail.com>
2020-05-03 15:49:54 +0200

    stdout and stderr

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Interpreter
 import System.Environment
+import System.IO (hPutStrLn, stderr, stdout)
 
 calcStr str = case calc str of
     Left err -> show err
@@ -10,4 +11,11 @@ calcStr str = case calc str of
 main = do
     args <- getArgs
     text <- readFile (head args)
-    putStrLn (calcStr text)
+    output <- calc text
+    let channel = case output of
+        Left err -> stderr
+        Right val -> stdout
+    let text = case output of
+        Left err -> show err
+        Right val -> show val
+    hPutStrLn channel output
